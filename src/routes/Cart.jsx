42e2165553da7d67cd45dcb3f934e59c99e17c64
Arz Shaheb <Ali Raza Shaheb>
2022-02-08 11:02:16 +0530

    validation added

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -2,7 +2,6 @@ import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import { rateFormatter } from '../api/RateFormatter';
 import { CartItem } from '../components/CartItem';
-import CheckOut from '../components/CheckOut';
 import { useGlobal } from '../context/Context';
 
 
